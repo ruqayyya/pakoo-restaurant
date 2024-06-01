332a28ee511f42d06cc0e910ee3e2eeb95332b12
ruqayyya <badirovaruqayya@gmail.com>
2024-06-01 18:00:11 +0400

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const About = () => {
-
   return (
     <div className="aboutPart">
       <div className="about">
-        Welcome to Pakoo restaurant. You can view the interior by turning the
-        pictures.
+        <p>
+          {" "}
+          Welcome to Pakoo restaurant. You can view the interior by turning the
+          pictures.
+        </p>
       </div>
-      <NavLink to={'/menu'}>
-      <button >menu</button>
+      <NavLink to={"/menu"}>
+        <button>menu</button>
       </NavLink>
     </div>
   );
